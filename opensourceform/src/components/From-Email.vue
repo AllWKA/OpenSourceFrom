40f5760b1a7bd28eb09120cde7ac27b908c415ea
AllWKA <bryanjaramillobaldeon@gmail.com>
2019-08-14 20:37:54 +0100

    starting form

---
+++ opensourceform/src/components/From-Email.vue
@@ -0,0 +1,96 @@
+<template>
+  <div class="form-email-container">
+    <label>Email:</label>
+    <input placeholder="placeHolder" type="email" />
+  </div>
+</template>
+
+<script>
+export default {
+  name: "form-email",
+  props: {
+    msg: String
+  },
+  data(){
+    return {
+      disabled:{
+        exist:false,
+        value:""
+      },
+      max:{
+        exist:false,
+        value:""
+      },
+      step:{
+        exist:false,
+        value:""
+      },
+      name:{
+        exist:false,
+        value:""
+      },
+      value:{
+        exist:false,
+        value:""
+      },
+      email:{
+        exist:false,
+        value:""
+      },
+      placeHolder:{
+        exist:false,
+        value:""
+      },
+      maxlength:{
+        exist:false,
+        value:""
+      },
+      minlength:{
+        exist:false,
+        value:""
+      },
+      multiple:{
+        exist:false,
+        value:""
+      },
+      pattern: {
+        exist:false,
+        value:""
+      },
+      readonly:{
+        exist:false,
+        value:""
+      },
+      spellcheck:{
+        exist:false,
+        value:""
+      }
+    }
+  },
+  methods:{
+    initializeSttributes() {
+      
+    },
+    isDisable(){
+      
+    }
+  },
+  watch:{
+
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.form-email-container {
+  display: flex;
+  flex-flow: row;
+}
+.form-email-container label {
+  flex-basis: 25%;
+}
+.form-email-container input {
+  flex-basis: 75%;
+}
+</style>
