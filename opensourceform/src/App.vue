47df8536ae6b877d500ff23fe85d1a1c8bda2736
unknown <laura.kisima@gmail.com>
2019-08-15 09:32:07 +0100

    he hecho el nombre y apellidos del formulario, con sus respectivos métodos

--- opensourceform/src/App.vue
+++ opensourceform/src/App.vue
@@ -1,17 +1,22 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <nombreYApellidos  :user =user />
+    {{user}}
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import nombreYApellidos from './components/nombre-y-apellidos.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    nombreYApellidos
+  },
+  data () {
+    return {
+      user : {}
+    }
   }
 }
 </script>
--- opensourceform/src/components/nombre-y-apellidos.vue
+++ opensourceform/src/components/nombre-y-apellidos.vue
@@ -1,30 +1,41 @@
 <template>
   <div class="form-nombre-y-apellidos" >
-    <input type="text" placeholder="nombre" v-model="nombre">
-    <p> {{}} </p>
-    <input type="text" placeholder="apellidos" v-model="apellido">
-    <p> </p>
+    <input type="text" placeholder="nombre" v-model="user.nombre">
+    <p v-if="user.invalid === true"> nombre inválido </p>
+    <input type="text" placeholder="apellido" v-model="user.apellido">
+    <p v-if="user.invalid2 === true"> Apellido inválido </p>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'HelloWorld',
+  name: 'nombreYApellidos',
   props: {
-    nombre: "",
-    apellido: ""
+    user:{
+      nombre: String,
+      apellido: String,
+      invalid: Boolean,
+      invalid2: Boolean
+    }
+    
   },
   watch: {
-    validacion(){
-       if (typeof nombre != "string") {
-         
+    "user.nombre" () {
+       if ( typeof this.user.nombre != "string" || this.user.nombre === "" ) {
+         return this.user.invalid = true
+        }
+      },
+    "user.apellido" () {
+       if ( typeof this.user.apellido != "string" || this.user.apellido === "" ) {
+         return this.user.invalid2 = true
+        }
     }
   }
-}    
 
+}
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
+//<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
 h3 {
   margin: 40px 0 0;
