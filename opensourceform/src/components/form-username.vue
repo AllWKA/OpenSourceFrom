bf93b55c5201ea6b41b4989624356b2b40fb89ac
Thais <thaishdzlpz@gmail.com>
2019-08-15 11:22:29 +0100

    Funka

--- opensourceform/src/components/form-username.vue
+++ opensourceform/src/components/form-username.vue
@@ -1,8 +1,9 @@
 <template>
     <div class="username">
       <label for="username">Username</label>
-      <input type="text" v-model="username" placeholder="Username">
-      <p>{{  username.split('').includes('a')   }}</p>
+      <input type="text" v-model="username" placeholder="Username" v-on:keyup.enter="checkUsername()">
+      <p>{{  error }}</p>
+      
     </div>
 </template>
 
@@ -10,41 +11,53 @@
 export default {
   name: 'username',
   props: {
-    username: String
+    // username: String
+  },
+
+  data: function() {
+    return {
+
+      username : '',
+      error : '',
+      letters: ['a','b','c'],
+      words : [
+        'thais',
+        'david',
+        'fran'
+      ]
+    }
   },
 
   methods : {
-
-    checkUsername() {
-      
-    },
-    checkLength() {
-      if (this.username.length <= 1 || this.username.length >= 50) {
-        return false;
+    checkUsername(){
+      this.checkWords(this.words);
+      if (this.username.length <= 1 || this.username.length > 20) {
+        this.error = "El username es demasiado corto";
+        return;
       }
     },
     checkLetters(letters) {
-      arrayLettersUsername = this.username.split('');
+      let arrayLettersUsername = this.username.split('');
       arrayLettersUsername.forEach(letritaUser => {
         letters.forEach(letrita =>{
           if (letritaUser == letrita) {
-            return false;
-          }else{
-            return true;
+            this.error = "El Username tiene una letra no valida";
+            return;
           }
         })
       });
-
     },
     checkWords(words) {
       
+      if (words.indexOf(this.username) == -1) {
+        this.error='';
+        this.error = "El UsernamePalabreja es válido";
+      }else{
+        this.error = "El Username no es válido";
+      }
     }
-
-
   }
-}
-
-
+}   
 </script>
 
 
